71009563daa2e15ba6cbb31f4e635dafa2a700a0
John Obradovic <jgobradovic@gmail.com>
2018-06-21 12:49:02 -0700

    Beautiful seelction of jigged addresses.

--- jig.js
+++ jig.js
@@ -8,13 +8,16 @@ translations = [
   ["north", "n.", "n"],
   ["south", "s.", "s"],
   ["east", "e.", "e"],
-  ["boulevard", "blvd", "blvd."]
+  ["boulevard", "blvd", "blvd."],
+  ["mountain", "mtn.", "mtn"]
 ];
 
 units = ["Unit A", "Unit B", "Unit C"];
 
+prefixes = ["ABC", "EFG", "ABD", "XYZ", "DEF", "GHI"];
+
 function jigAddress(addressString) {
-  jigs = [{ address: addressString, others: [] }];
+  jigs = new Set([addressString]);
   parts = addressString.split(" ");
   for (var index = 0; index < parts.length; index++) {
     part = parts[index];
@@ -32,31 +35,61 @@ function jigAddress(addressString) {
       possibleJigs.forEach(jig => {
         jigs.forEach(address => {
           fullJig = "";
-          valueParts = address["address"].split(" ");
+          valueParts = address.split(" ");
           for (var i = 0; i < valueParts.length; i++) {
             fullJig += (i !== index ? valueParts[i] : jig) + " ";
           }
           fullJig = fullJig.substring(0, fullJig.length - 1);
-          exists = false;
-          jigs.forEach(existingAddress => {
-            if (existingAddress["address"] === fullJig) {
-              exists = true;
-            }
-          });
-          if (!exists) {
-            jigs.push({ address: fullJig, others: [] });
-          }
+          jigs.add(fullJig);
         });
       });
     });
+    if (parts.length >= 3) {
+      jigs.forEach(address => {
+        if (!address.includes("\n")) {
+          addressParts = address.split(" ");
+          for (var i = 2; i < addressParts.length; i++) {
+            splitAddress = "";
+            for (var j = 0; j < i; j++) {
+              splitAddress += addressParts[j] + " ";
+            }
+            splitAddress =
+              splitAddress.substring(0, splitAddress.length - 1) + "\n";
+            for (var j = i; j < addressParts.length; j++) {
+              splitAddress += addressParts[j] + " ";
+            }
+            jigs.add(splitAddress.substring(0, splitAddress.length - 1));
+          }
+        }
+      });
+    }
   }
-  jigs.forEach(address => {
-    jig = address["address"];
-    if (!jig.includes("\n")) {
+  return jigs;
+}
+
+function addUnitsToAddresses(addresses) {
+  unitAddresses = new Set();
+  addresses.forEach(address => {
+    if (!address.includes("\n")) {
       units.forEach(unit => {
-        address["others"].push(jig + "\n" + unit);
+        unitAddresses.add(address + "\n" + unit);
       });
     }
   });
-  return jigs;
+  return unitAddresses;
+}
+
+function addPrefixesToAddresses(addresses) {
+  newAddresses = new Set();
+  addresses.forEach(address => {
+    addressParts = address.split("\n");
+    prefixes.forEach(prefix => {
+      newAddress = prefix + " " + addressParts[0];
+      for (var i = 1; i < addressParts.length; i++) {
+        newAddress += "\n" + addressParts[i];
+      }
+      newAddresses.add(newAddress);
+    });
+  });
+  return newAddresses;
 }
